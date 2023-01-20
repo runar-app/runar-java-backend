90fbe5f2cbe4cc7b786616984c6622f76c18694d
Alexandr Dmowski <dmowski@yandex.ru>
2023-01-20 18:43:26 +0100

    TMP

--- src/main/java/com/developmate/demo/controller/DocumentController.java
+++ src/main/java/com/developmate/demo/controller/DocumentController.java
@@ -1,42 +1,20 @@
 package com.developmate.demo.controller;
 
-import com.developmate.demo.model.Document;
-import com.developmate.demo.repository.DocumentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class DocumentController {
-    private final DocumentRepository repository;
-    public DocumentController(DocumentRepository repository) {
-        this.repository = repository;
-    }
-    @GetMapping("/menu")
-    public List<Document> getRootMenu() {
-        return repository.findByType("root").stream()
-                .sorted(Comparator.comparing(Document::getSortOrder))
-                .toList();
+    public DocumentController() {
     }
 
     @GetMapping("/")
     public String getHelloWorld() {
         return "Hey";
     }
-    @GetMapping("/menu/{id}")
-    public List<Document> getSubMenus(@PathVariable("id") String id) {
-        Optional<Document> documents = repository.findBy_id(id);
-        return documents.map(value -> value.getChildIds().stream()
-                .map(repository::findBy_id)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .sorted(Comparator.comparing(Document::getSortOrder))
-                .toList()).orElseGet(ArrayList::new);
-    }
-
 }
