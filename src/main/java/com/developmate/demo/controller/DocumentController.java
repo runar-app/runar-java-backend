e22d6cb4079b540cf0d5bdaf3983c904b17df76f
Starbreaker84 <romalaev@gmail.com>
2023-01-21 19:19:36 +0300

    Add Document controller class

--- src/main/java/com/developmate/demo/controller/DocumentController.java
+++ src/main/java/com/developmate/demo/controller/DocumentController.java
@@ -1,25 +1,35 @@
 package com.developmate.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.developmate.demo.model.Document;
+import com.developmate.demo.repository.DocumentRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v2")
 public class DocumentController {
-    public DocumentController() {
+    private final DocumentRepository repository;
+    public DocumentController(DocumentRepository repository) {
+        this.repository = repository;
     }
-
-    @GetMapping("/")
-    public String getHelloWorld() {
-        return "Root endpoint";
+    @GetMapping(value = "/", produces = "application/json")
+    public List<Document> getRootMenu() {
+        return repository.findByType("root").stream()
+                .sorted(Comparator.comparing(Document::getSortOrder))
+                .toList();
     }
-
-    @GetMapping("/test")
-    public String getMenu() {
-        return "Server works!";
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public List<Document> getSubMenus(@PathVariable("id") String id) {
+        Optional<Document> documents = repository.findBy_id(id);
+        return documents.map(value -> value.getChildIds().stream()
+                .map(repository::findBy_id)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .sorted(Comparator.comparing(Document::getSortOrder))
+                .toList()).orElseGet(ArrayList::new);
     }
 }
