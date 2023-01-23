a4b9a983126c1e1b064a699e31ddf29cbfaa7120
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-01-23 14:01:03 +0300

    Heroku with db (#5)
    * Add application.yaml
    
    * Add Document class
    
    * Add Document controller class
    
    * Add Document repository class
    
    * Add Spring Data Mongo DB dependency
    
    * Change How to use section
    
    * Change How to use section again
    
    * Standardize method name in Repository (find by id)
    
    * Move the database uri to an environment variable

---
+++ src/main/java/com/developmate/demo/repository/DocumentRepository.java
@@ -0,0 +1,14 @@
+package com.developmate.demo.repository;
+
+import com.developmate.demo.model.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DocumentRepository extends MongoRepository<Document, String> {
+    List<Document> findByType(String type);
+    Optional<Document> findById(String id);
+}
